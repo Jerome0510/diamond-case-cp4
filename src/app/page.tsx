0c1c22a24f033e676596b16bf2e76daaa0c3773b
Jerome Marbach <jeromemarbach@outlook.fr>
2025-07-23 16:31:47 +0200

    create illustration for page accueil

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import styles from "./page.module.css";
+import Accueil from "@/component/Accueil";
 
 const Home = () => {
-  return <section className={styles.homeBody}></section>;
+  return (
+    <section className={styles.homeBody}>
+      <Accueil />
+    </section>
+  );
 };
 
 export default Home;

4fecc7e34b9b229663d0456f35c53191916ff03c
Jerome Marbach <jeromemarbach@outlook.fr>
2025-07-23 15:24:16 +0200

    create logo in navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 import styles from "./page.module.css";
 
 const Home = () => {
-  return (
-    <section className={styles.homeBody}>
-      <h1>titre</h1>
-    </section>
-  );
+  return <section className={styles.homeBody}></section>;
 };
 
 export default Home;
--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -3,21 +3,20 @@ import styles from "./Header.module.css";
 const Header = () => {
   return (
     <header className={styles.globalHeader}>
+      <div className={styles.logoSection}>
+        <img
+          className={styles.logoBalise}
+          src="/logo/baliseHtmlBlanche.PNG"
+          alt="LOGO"
+        />
+        <h2 className={styles.logoName}>DIAMOND CASE</h2>
+      </div>
       <ul className={styles.menuNavBar}>
         <li className={styles.navBarList}>
-          <button className={styles.buttonGarment}>POLOS</button>
+          <button className={styles.buttonGarment}>NOS POLOS</button>
         </li>
         <li className={styles.navBarList}>
-          <button className={styles.buttonGarment}>PANTALONS</button>
-        </li>
-        <li className={styles.navBarList}>
-          <button className={styles.buttonGarment}>CHEMISES</button>
-        </li>
-        <li className={styles.navBarList}>
-          <button className={styles.buttonGarment}>PULLS</button>
-        </li>
-        <li className={styles.navBarList}>
-          <button className={styles.buttonGarment}>CHAUSSURES</button>
+          <button className={styles.buttonGarment}>NOS MONTRES</button>
         </li>
       </ul>
     </header>
